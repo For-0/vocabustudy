adfe626c3408f15e3ee51b6cedfb53db6faf5fbe
DG3QRT1 <115378514+DG3QRT1@users.noreply.github.com>
2022-11-17 09:51:10 -0600

    Added collapsible script

--- src/js/help-center.js
+++ src/js/help-center.js
@@ -1,2 +1,15 @@
 import initialize from "./general";
 initialize();
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+coll[i].addEventListener("click", function() {
+this.classList.toggle("active");
+var content = this.nextElementSibling;
+if (content.style.maxHeight){
+content.style.maxHeight = null;
+} else {
+content.style.maxHeight = content.scrollHeight + "px";
+}
+});
+}
