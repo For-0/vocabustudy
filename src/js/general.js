6e9a0a2be8f21ab8501cf7a428091f8a9eaa2f6e
Siddhant <115331356+grimsteel@users.noreply.github.com>
2022-12-15 15:24:01 +0000

    Gitpod emulator ports

--- src/js/general.js
+++ src/js/general.js
@@ -52,6 +52,9 @@ export default function initialize(authStateChangedCallback = () => {}, remoteCo
     } else if (process.env.CODESPACES) {
         connectAuthEmulator(auth, `https://${process.env.CODESPACE_NAME}-9099.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}/:443`, {disableWarnings: true});
         connectFirestoreEmulator(db, `${process.env.CODESPACE_NAME}-8080.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`, 443);
+    } else if (process.env.GITPOD_WORKSPACE_URL) {
+        connectAuthEmulator(auth, `https://${9099}-${process.env.GITPOD_WORKSPACE_URL.replace("https://", "")}/:443`, {disableWarnings: true});
+        connectFirestoreEmulator(db, `${8080}-${process.env.GITPOD_WORKSPACE_URL.replace("https://", "")}`, 443);
     }
     window.signOut = () => auth.signOut(); // TODO move all to SW, as this will not work on some pages. ALso may disable tree shaking
     auth.onAuthStateChanged(async user => {
