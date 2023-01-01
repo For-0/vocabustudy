80383c09857801c0fecb984907e44bf5c7b13c55
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-01-01 00:38:57 -0600

    Credits on dark theme and sentry versioning

--- src/js/sentry.js
+++ src/js/sentry.js
@@ -6,7 +6,7 @@ if (!localStorage.getItem("sentry_optout")) {
         dsn: "https://4439a6b734334a92b39a9aabfb1bebd4@o4504087298048000.ingest.sentry.io/4504087320723456",
         integrations: [new BrowserTracing()],
         tracesSampleRate: 1.0,
-        release: `vocabustudy@${process.env.npm_package_version}`
+        release: `vocabustudy@v${process.env.npm_package_version}`
     });
     window.sentryCaptureException = Sentry.captureException;
     /** @param {import("firebase/auth").User?} user */
