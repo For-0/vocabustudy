68785c0648b197cebc91078d883016397720df51
Siddhant <115331356+grimsteel@users.noreply.github.com>
2022-12-31 22:10:48 -0600

    Service worker, runtime env badge

---
+++ src/service-worker.js
@@ -0,0 +1,14 @@
+import {manifest, version} from "@parcel/service-worker";
+
+async function install() {
+    const cache = await caches.open(version);
+    //await cache.addAll(manifest);
+}
+
+async function activate() {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(key => key !== version && caches.delete(key)));
+}
+
+addEventListener("install", e => e.waitUntil(install()));
+addEventListener("activate", e => e.waitUntil(activate()));
