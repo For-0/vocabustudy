220223ccbcc02581251d35f4416cb5e26cabec12
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-01-07 00:56:56 -0600

    Minor fixes
    * style
    * add sentry prefix
    * upgrade to checkout v3 on lint
    * remove PII from sentry

--- src/js/sentry.js
+++ src/js/sentry.js
@@ -7,9 +7,10 @@ if (!localStorage.getItem("sentry_optout")) {
         dsn: "https://4439a6b734334a92b39a9aabfb1bebd4@o4504087298048000.ingest.sentry.io/4504087320723456",
         integrations: [new BrowserTracing()],
         tracesSampleRate: 1.0,
-        release: `vocabustudy@v${npm_package_version}`
+        release: `vocabustudy@v${npm_package_version}`,
+        ignoreErrors: ["AbortError", "Display name too long", "Failed to register a service worker", "auth/network-request-failed", /Failed to access \w+ before initialization/]
     });
     window.sentryCaptureException = Sentry.captureException;
     /** @param {import("firebase/auth").User?} user */
-    window.sentrySetUser = user => Sentry.setUser(user ? {email: user.email, id: user.uid, username: user.displayName} : null);
+    window.sentrySetUser = user => Sentry.setUser(user ? {id: user.uid} : null);
 }
