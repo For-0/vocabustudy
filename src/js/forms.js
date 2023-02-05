f800f71f9d9537c95044d657bc1b35b10c218911
Ruhan Gupta <ruhan_gupta@outlook.com>
2023-02-05 02:27:34 -0600

    Siddhant's requests
    made the code look better lol

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,160 +1,114 @@
 import { collection, addDoc } from "firebase/firestore/lite";
 import initialize from "./general.js";
 
-const feedbackName = document.getElementById("name-feedback");
-const feedbackEmail = document.getElementById("email-feedback");
-const feedbackRating = document.getElementById("rating-feedback");
-const feedbackText = document.getElementById("text-feedback");
-const feedbackBtn = document.getElementById("btn-feedback");
-
-const bugName = document.getElementById("name-bug");
-const bugEmail = document.getElementById("email-bug");
-const bugURL = document.getElementById("url-bug");
-const bugText = document.getElementById("text-bug");
-const bugBtn = document.getElementById("btn-bug");
-
-const takedownName = document.getElementById("name-takedown");
-const takedownEmail = document.getElementById("email-takedown");
-const takedownURL = document.getElementById("url-takedown");
-const takedownText = document.getElementById("text-takedown");
-const takedownBtn = document.getElementById("btn-takedown");
-
-const otherName = document.getElementById("name-other");
-const otherEmail = document.getElementById("email-other");
-const otherText = document.getElementById("text-other");
-const otherBtn = document.getElementById("btn-other");
-
-const toFeedback = document.getElementById("to-feedback");
-const toBug = document.getElementById("to-bug");
-const toTakedown = document.getElementById("to-takedown");
-const toOther = document.getElementById("to-other");
-
-const feedbackForm = document.getElementById("feedback-form");
-const bugForm = document.getElementById("bug-form");
-const takedownForm = document.getElementById("takedown-form");
-const otherForm = document.getElementById("other-form");
+// collapse the below code into a single const array
+const forms = {
+    feedback: {
+        name: document.getElementById("name-feedback"),
+        email: document.getElementById("email-feedback"),
+        rating: document.getElementById("rating-feedback"),
+        text: document.getElementById("text-feedback"),
+        btn: document.getElementById("btn-feedback"),
+        form: document.getElementById("feedback-form"),
+        function: sendFeedback
+    },
+    bug: {
+        name: document.getElementById("name-bug"),
+        email: document.getElementById("email-bug"),
+        url: document.getElementById("url-bug"),
+        text: document.getElementById("text-bug"),
+        btn: document.getElementById("btn-bug"),
+        form: document.getElementById("bug-form"),
+        function: sendBug
+    },
+    takedown: {
+        name: document.getElementById("name-takedown"),
+        email: document.getElementById("email-takedown"),
+        url: document.getElementById("url-takedown"),
+        text: document.getElementById("text-takedown"),
+        btn: document.getElementById("btn-takedown"),
+        form: document.getElementById("takedown-form"),
+        function: sendTakedown
+    },
+    other: {
+        name: document.getElementById("name-other"),
+        email: document.getElementById("email-other"),
+        text: document.getElementById("text-other"),
+        btn: document.getElementById("btn-other"),
+        form: document.getElementById("other-form"),
+        function: sendOther
+    }
+};
 
 const {db, auth} = initialize(async user => {
     if (user) {
-        feedbackForm.addEventListener('submit',  async (e) => {
-            e.preventDefault();
-            feedbackBtn.classList.add("is-loading");
-            await sendFeedback();
-        });
-        bugForm.addEventListener('submit', async (e) => {
-            e.preventDefault();
-            bugBtn.classList.add("is-loading");
-            await sendBug();
-        });
-        takedownForm.addEventListener('submit', async (e) => {
-            e.preventDefault();
-            takedownBtn.classList.add("is-loading");
-            await sendTakedown();
-        });
-        otherForm.addEventListener('submit', async (e) => {
-            e.preventDefault();
-            otherBtn.classList.add("is-loading");
-            await sendOther();
+        Object.values(forms).forEach(form => {
+            form.form.addEventListener('submit', async (e) => {
+                e.preventDefault();
+                form.btn.classList.add("is-loading");
+                await form.function();
+            });
         });
     } else {
-        toFeedback.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
-        toBug.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
-        toTakedown.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
-        toOther.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
-        feedbackBtn.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
-        bugBtn.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
-        takedownBtn.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
-        otherBtn.addEventListener("click", async (e) => {
-            e.preventDefault();
-            localStorage.setItem("redirect_after_login", location.href);
-            location.href = "/#login";
-        });
+        localStorage.setItem("redirect_after_login", location.href);
+        location.href = "/#login";
     }
 });
 
 async function sendFeedback() {
     const feedback = {
-        name: feedbackName.value,
-        email: feedbackEmail.value,
-        rating: feedbackRating.value,
-        text: feedbackText.value,
+        name: forms.feedback.name.value,
+        email: forms.feedback.email.value,
+        rating: forms.feedback.rating.value,
+        text: forms.feedback.text.value,
         uid: auth.currentUser.uid
     };
     await addDoc(collection(db, "form_data", "types", "feedback"), feedback).then(() => {
-        feedbackBtn.classList.remove("is-loading");
-        feedbackBtn.disabled = true;
-        feedbackBtn.innerHTML = "Submitted!";
+        forms.feedback.btn.classList.remove("is-loading");
+        forms.feedback.btn.disabled = true;
+        forms.feedback.btn.innerText = "Submitted!";
     })
 }
 
 async function sendBug() {
     const bug = {
-        name: bugName.value,
-        email: bugEmail.value,
-        url: bugURL.value,
-        text: bugText.value,
+        name: forms.bug.name.value,
+        email: forms.bug.email.value,
+        text: forms.bug.text.value,
         uid: auth.currentUser.uid
     };
     await addDoc(collection(db, "form_data", "types", "bug"), bug).then(() => {
-        bugBtn.classList.remove("is-loading");
-        bugBtn.disabled = true;
-        bugBtn.innerHTML = "Submitted!";
+        forms.bug.btn.classList.remove("is-loading");
+        forms.bug.btn.disabled = true;
+        forms.bug.btn.innerText = "Submitted!";
     })
 }
 
 async function sendTakedown() {
     const takedown = {
-        name: takedownName.value,
-        email: takedownEmail.value,
-        url: takedownURL.value,
-        text: takedownText.value,
+        name: forms.takedown.name.value,
+        email: forms.takedown.email.value,
+        url: forms.takedown.url.value,
+        text: forms.takedown.text.value,
         uid: auth.currentUser.uid
     };
     await addDoc(collection(db, "form_data", "types", "takedown"), takedown).then(() => {
-        takedownBtn.classList.remove("is-loading");
-        takedownBtn.disabled = true;
-        takedownBtn.innerHTML = "Submitted!";
+        forms.takedown.btn.classList.remove("is-loading");
+        forms.takedown.btn.disabled = true;
+        forms.takedown.btn.innerText = "Submitted!";
     })
 }
 
 async function sendOther() {
     const other = {
-        name: otherName.value,
-        email: otherEmail.value,
-        text: otherText.value,
+        name: forms.other.name.value,
+        email: forms.other.email.value,
+        text: forms.other.text.value,
         uid: auth.currentUser.uid
     };
     await addDoc(collection(db, "form_data", "types", "other"), other).then(() => {
-        otherBtn.classList.remove("is-loading");
-        otherBtn.disabled = true;
-        otherBtn.innerHTML = "Submitted!";
+        forms.other.btn.classList.remove("is-loading");
+        forms.other.btn.disabled = true;
+        forms.other.btn.innerText = "Submitted!";
     })
 }
