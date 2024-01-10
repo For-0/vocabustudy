d5bf8f51c126fd9a5048f457e4f0582321c3bc75
Siddhant <115331356+grimsteel@users.noreply.github.com>
2024-01-09 22:00:36 -0600

    Math support with KaTeX

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,12 +1,86 @@
-import { marked } from "marked";
-import DOMPurify, { type SanitizeAttributeHookEvent } from "dompurify";
+import { marked, type RendererExtension, type TokenizerExtension } from "marked";
+import katex from "katex";
+import DOMPurify, { type Config, type SanitizeAttributeHookEvent } from "dompurify";
 
-const allowedClassesRE = /^(text-[a-z]+-\d+|bg-[a-z]+-\d+|font-(?:normal|medium|semibold|bold)|text-(?:sm|base|lg|xl|2xl))$/i;
+const allowedClassesRE = /^(dark:)?(text-[a-z]+-\d+|bg-[a-z]+-\d+|font-(?:normal|medium|semibold|bold)|text-(?:sm|base|lg|xl|2xl)|rounded-(sm|md|lg)|p\w?-\d+|m\w?-\d+)$/i;
 const disallowedClasses: string[] = [];
-const purifyOptions = { FORBID_ATTR: ["style"], FORBID_TAGS: ["style"] };
+const purifyOptions: Config = { FORBID_ATTR: ["style"], FORBID_TAGS: ["style"], ADD_TAGS: ["annotation", "semantics"] };
 
 const mainPurify = DOMPurify();
 const extractImagesPurify = DOMPurify();
+
+function walkLatex(src: string, delim: [string, string]): { raw: string, contents: string } | undefined {
+    // start iterating through the string
+    let braceLevel = 0;
+    for (let i = delim[0].length; i < src.length; i++) {
+        // Found closing delimiter
+        if (src.slice(i, i + delim[1].length) === delim[1] && braceLevel <= 0) {
+            const latex = src.slice(delim[0].length, i);
+            return { contents: latex, raw: src.slice(0, i + delim[1].length) };
+        }
+        switch (src[i]) {
+            // Skip over escaped characters
+            case "\\": {
+                i++;
+                break;
+            }
+            case "{": {
+                braceLevel++;
+                break;
+            }
+            case "}": {
+                braceLevel--;
+                break;
+            }
+        }
+    }
+}
+
+function makeLatexExtension(name: string, delims: [string, string][], displayMode: boolean): TokenizerExtension & RendererExtension {
+    return {
+        name,
+        level: displayMode ? "block" : "inline",
+        start(src) {
+            for (const delim of delims) {
+                // Check if the delim is in the string and is part of a full latex expression
+                const i = src.indexOf(delim[0]);
+                if (i !== -1 && walkLatex(src.substring(i), delim)) return i;
+            }
+            return -1;
+        },
+        tokenizer(src, _tokens) {
+            // Find the delim at the start
+            const delim = delims.find(delim => src.startsWith(delim[0]));
+            if (delim) {
+                // Parse the latex our
+                const latex = walkLatex(src, delim);
+                if (latex)
+                    return {
+                        type: name,
+                        ...latex,
+                        tokens: []
+                    }
+            }
+        },
+        renderer(token) {
+            try {
+                return katex.renderToString(token.contents as string, { displayMode, output: "mathml" });
+            } catch (e) {
+                console.log(JSON.stringify(e))
+                return `<div class="p-4 text-sm text-rose-800 rounded-lg bg-rose-50 dark:bg-zinc-800 dark:text-rose-400" role="alert">
+                    <span class="font-medium">KaTeX Error:</span> ${(e as { rawMessage: string}).rawMessage}
+                </div>`;
+            }
+        }
+    }
+}
+
+marked.use({
+    extensions: [
+        makeLatexExtension("latex-block", [["$$", "$$"], ["\\[", "\\]"]], true),
+        makeLatexExtension("latex-inline", [["$", "$"], ["\\(", "\\)"]], false)
+    ]
+})
 
 const nodeIsImage = (node: Element): node is HTMLImageElement => node.nodeName === "IMG";
 
